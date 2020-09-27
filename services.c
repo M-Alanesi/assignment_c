cf71e87b2a299f6df07302859478b0a771006d5a
M-Alanesi <71952105+M-Alanesi@users.noreply.github.com>
2020-09-27 04:23:09 +0300

    added display function for menu

--- services.c
+++ services.c
@@ -1,1 +1,4 @@
+void display () {
 
+ printf("Welcome to My application \n please enter a choice: \n 1.add two matrices \n2.subtract two matrics\n 3.Sum & Average \n 4.factorial\n 5.Largest & Smallest \n 6.Delete data\n 7.exit\n");
+}
