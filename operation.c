340b3ecac3ed6248c5a799e8c24751bb1420f58e
M-Alanesi <71952105+M-Alanesi@users.noreply.github.com>
2020-09-27 04:20:57 +0300

    added c main function

--- operation.c
+++ operation.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+#include "services.c"
+
+
+int main()
+{
+ 
+
+
+      running(); // this makes the whole program run and the number 1 , 2 is just to make the function dynamic
+
+
+}
