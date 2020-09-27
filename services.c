5e41b4970515b21a0be37d8279bdffe46acc002f
M-Alanesi <71952105+M-Alanesi@users.noreply.github.com>
2020-09-27 04:23:43 +0300

    added function to display any matrix

--- services.c
+++ services.c
@@ -2,3 +2,17 @@ void display () {
 
  printf("Welcome to My application \n please enter a choice: \n 1.add two matrices \n2.subtract two matrics\n 3.Sum & Average \n 4.factorial\n 5.Largest & Smallest \n 6.Delete data\n 7.exit\n");
 }
+void display_matrix(int x,int y,int arr[x][y]) {
+
+for (int c = 0; c < x; ++c) {
+        printf("|");
+
+  for (int i = 0; i < y; ++i) {
+    printf("%d ",arr[c][i]);
+
+  }
+  printf("|\n");
+}
+
+
+}
